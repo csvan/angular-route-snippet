c3bb7289b2ae032e61f931ac053d4605960810e1
csvan <christopher.svanefalk@gmail.com>
2014-11-21 14:22:44 +0100

    Update layout.

---
+++ app/js/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+    'ngRoute',
+    'myApp.version'
+]).
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'views/view1.html',
+            controller: 'View1Ctrl'
+        });
+        $routeProvider.when('/view2', {
+            templateUrl: 'views/view2.html',
+            controller: 'View2Ctrl'
+        });
+        $routeProvider.otherwise({redirectTo: '/view1'});
+    }])
+
+    .controller('View2Ctrl', [function () {
+
+    }])
+
+    .controller('View1Ctrl', [function () {
+
+    }]);
